1a623b65e856173e9fa564c923d7209400842e76
Samia <samia@gmail.com>
2025-12-04 13:50:18 +0300

    pandas attribute

--- ml.py
+++ ml.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 
 df = pd.read_csv('hotel_booking_data.txt')
-# print(df.head())
-print(df.to_string())
+# df.head(3)
+# print(df.head(10))
+# print(df.to_string())
 # print(df.shape)
 # print(df.info())
 # print(pd.options.display.max_columns)
+# print(df.tail)
+# print(df.dtypes)
+# print(df.columns)
+# print(df.describe())
+# print(df.min())
+# print(df.Guests)
+# print(df.Payment_Status,df.Guests)
+# print(df.isnull().sum)
+
--- nmp.py
+++ nmp.py
@@ -1,11 +1,21 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data=np.array([1,2,3,4,5])
+# data=np.array([1,2,3,4,5])
 # print(data)
 # print(type(data))
 # print(len(data))
 # print(data.shape)
 
-s=pd.Series(data)
-print(s)
+# s=pd.Series(data)
+# print(s)
+
+xpoints = np.array([0,3,5,7,9])
+ypoints = np.array([0,1,4,9,16])    
+
+plt.plot(xpoints, ypoints)
+# plt.plot(xpoints, ypoints,  'o')
+# plt.show()
+plt.savefig("plot.jpg")
+print("Plot saved as plot.jpg")
