4a4478bfa8847472fe9f52dc0660b15453d489c3
Samia <samia@gmail.com>
2025-12-11 23:28:56 +0300

    pandas attribute

--- task.py
+++ task.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("olympics.csv ")
+df = pd.read_csv("olympics.csv ", header=4)
 # print(df.head(10))
 # print(df.shape)
 # print(df.dtypes)
-print(df.columns)
+# print(df.columns)
+# print(df.describe)
+# print(df.info())
+# print(df.isna().sum)
+# print(df.dropna())
